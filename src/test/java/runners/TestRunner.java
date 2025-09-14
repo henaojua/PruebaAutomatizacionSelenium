73d44a6b3ea000953174330a2d2caeb7627c30e3
henaojua <henaoariza2@gmail.com>
2025-09-13 19:22:06 -0500

    Prueba Automatizacion

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -2,32 +2,22 @@ package runners;
 
 import io.cucumber.junit.CucumberOptions;
 import net.serenitybdd.cucumber.CucumberWithSerenity;
-import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
-import io.github.bonigarcia.wdm.WebDriverManager;
-
 
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
         features = "src/test/resources/features",
-        glue = {
-                "stepdefinitions",
-                "runners"
-        },
-        stepNotifications = true,
+        glue = {"stepdefinitions"},
         plugin = {
                 "pretty",
-                "html:target/cucumber-reports/cucumber.html",
-                "json:target/cucumber-reports/cucumber.json"
+                "html:build/cucumber-reports/cucumber.html",
+                "json:build/cucumber-reports/cucumber.json"
         },
         tags = ""
 )
 
 public class TestRunner {
 
-        @BeforeClass
-        public static void setup() {
-                WebDriverManager.chromedriver().setup();
-        }
+
 }
 
--- src/test/java/stepdefinitions/Caso1.java
+++ src/test/java/stepdefinitions/Caso1.java
@@ -7,11 +7,13 @@ import io.cucumber.java.en.*;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
 import net.serenitybdd.screenplay.actions.Open;
-import net.thucydides.core.annotations.Managed;
+import net.serenitybdd.annotations.Managed;
 import org.openqa.selenium.WebDriver;
 import tasks.RealizarStep;
 import tasks.SeleccionarOpcion;
 import tasks.SeleccionarProducto;
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 
 
